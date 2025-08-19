3b0b9d40f79217f0ca32acbc85d197b645aa43af
Subbu Muthiah <subbumuthiah1@gmail.com>
2025-08-18 23:50:01 -0500

    initial

---
+++ script.js
@@ -0,0 +1,21 @@
+window.onscroll = function() {
+    const nav = document.querySelector('.navbar');
+    if(this.scrollY <= 75) {
+        nav.className = 'navbar fixed-top navbar-expand-lg navbar-dark bg-transparent';
+    } else {
+        nav.className = 'navbar fixed-top navbar-expand-lg navbar-dark bg-dark';
+    }
+};
+
+function detectMob() {
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+    ];
+    
+    return toMatch.some((toMatchItem) => {
+        return navigator.userAgent.match(toMatchItem);
+    });
+}
