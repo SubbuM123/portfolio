f017f484cc02e679584c75d70610a5205d7dd7f5
Subbu Muthiah <subbumuthiah1@gmail.com>
2026-07-11 23:30:48 -0500

    updates

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ function detectMob() {
         /webOS/i,
         /iPhone/i,
         /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
     ];
     
     return toMatch.some((toMatchItem) => {
